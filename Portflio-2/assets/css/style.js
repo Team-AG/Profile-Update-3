a00c75d96947e50825053348d5b64d7f0f6a5b7a
ANDREW <66812117+Team-AG@users.noreply.github.com>
2021-02-12 03:55:32 -0500

    Add files via upload

---
+++ Portflio-2/assets/css/style.js
@@ -0,0 +1,11 @@
+<script language="JavaScript" type="text/javascript">
+$('.navbar').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+
+</script>
