e9c8b6dbe9bc84b4392dd0bd2e238d8c77627caa
ANDREW <66812117+Team-AG@users.noreply.github.com>
2021-02-11 22:16:49 -0500

    Add files via upload

---
+++ assets/javascript/certs-section.js
@@ -0,0 +1,23 @@
+// THIS IS AN OWL CAROUSEL
+// LINKED WITH https://cdnjs.com/
+
+const movingImages = $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        }
+    }
+});
